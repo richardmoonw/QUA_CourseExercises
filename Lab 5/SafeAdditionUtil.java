a21dc0fa887b8607d473e7d389d07db4dcd072d9
Ricardo Luna <36082518+richardmoonw@users.noreply.github.com>
2020-03-29 20:31:27 -0700

    Create SafeAdditionUtil.java

---
+++ Lab 5/SafeAdditionUtil.java
@@ -0,0 +1,14 @@
+package mx.tec.lab.util;
+
+public class SafeAdditionUtil {
+	public int safeAdd(int a, int b) {
+		long result = ((long) a) + b;
+		
+		if(result > Integer.MAX_VALUE) {
+			return Integer.MAX_VALUE;
+		} else if (result < Integer.MIN_VALUE) {
+			return Integer.MIN_VALUE;
+		}
+		return (int) result;
+	}
+}
